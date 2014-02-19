7e7adfba817a846d91f2c299268bc2dfcff50910
Donnchadh O h-Ainle <xhanley@gmail.com>
2014-02-19 20:29:33 +1100

    Initialise repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Phinanceapp::Application.config.secret_key_base = 'dd0488f56ce69b8d8f24b0e645a6c79fc5a204a7375380914d54fa5fb2c9e3d2a897b0daecc86689cc5fcdfa79aea2dcc74c769eb3e576889111a2b7da05c7cc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Phinanceapp::Application.config.session_store :cookie_store, key: '_phinanceapp_session'
